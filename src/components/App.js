46c809820950df66eda16d207910b4408a89fdec
George-Okumu <gokumu12@gmail.com>
2022-04-18 15:57:09 +0300

    fetched data, deleted

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
-  return (
-    <div className="app">
-      <Header />
-      <ListingsContainer />
-    </div>
-  );
+  const [listings, setListings] = useState([]);
+  const [search, setSearch] = useState("");
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  //Display listings
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(response => response.json())
+      .then((data) => {
+        setIsLoaded(true);
+        setListings(data);
+      },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      )
+  }, []);
+
+  //search 
+  const displayedListings = listings.filter((listing) =>
+  listing.description.toLowerCase().includes(search.toLowerCase())
+);
+
+  //Delete single listing
+  function deleteSingleList(id, description) {
+    fetch(`http://localhost:6001/listings/${id}`, {
+      method: 'DELETE'
+    }).then(response => {
+      return response.json()
+    }).then(() => {
+      const updatedListings = listings.filter((list) => list.id !== id);
+      setListings(updatedListings);
+    });
+    alert(`${description} deleted successfully`);
+  }
+  if (error) {
+    return <div> Error: {error.message}</div>
+  } else if (!isLoaded) {
+    return <div>Loading...</div>
+  } else {
+    return (
+      <div className="app">
+        <Header searchListing={setSearch}/>
+        <ListingsContainer listings={displayedListings} handleDelete={deleteSingleList} />
+
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,21 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-function ListingCard() {
+function ListingCard({list, handleDelete}) {
+  //desctructure
+  let {description, image, location, id} = list;
+  const [like, setLike] = useState(false);
+
+  function deleteListing(){
+    handleDelete(id, description);
+  }
+
+
   return (
     <li className="card">
       <div className="image">
         <span className="price">$0</span>
-        <img src={"https://via.placeholder.com/300x300"} alt={"description"} />
+        <img src={image} alt={description} />
       </div>
       <div className="details">
-        {true ? (
-          <button className="emoji-button favorite active">★</button>
+        {like ? (
+          <button className="emoji-button favorite active" onClick={() => setLike(false)}>★</button>
         ) : (
-          <button className="emoji-button favorite">☆</button>
+          <button className="emoji-button favorite" onClick={() => setLike(true)}>☆</button>
         )}
-        <strong>{"description"}</strong>
-        <span> · {"location"}</span>
-        <button className="emoji-button delete">🗑</button>
+        <strong>{description}</strong>
+        <span> · {location}</span>
+        <button className="emoji-button delete" onClick={deleteListing}>🗑</button>
       </div>
     </li>
   );
